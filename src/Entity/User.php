68b69d2aaebaa5b1e2fc7378cbef55ecbd4aa724
ilyes200876 <ilyes200876@live.fr>
2023-11-24 13:10:46 +0100

    update serialization

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -17,11 +17,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['allUsers','oneUser'])]
+    #[Groups(['user'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
-    #[Groups(['allUsers','oneUser'])]
+    #[Groups(['user'])]
     private ?string $email = null;
 
     #[ORM\Column]
